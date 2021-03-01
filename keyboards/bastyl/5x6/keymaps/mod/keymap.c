25931ba0d29efc7fe36baa5a0d283296285993de
Rafal Wachol <gorudonu@gmail.com>
2021-03-01 03:17:34 +0000

    adjusted bastyl

--- keyboards/bastyl/5x6/keymaps/mod/keymap.c
+++ keyboards/bastyl/5x6/keymaps/mod/keymap.c
@@ -57,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB, KC_Q, KC_W,  KC_E, KC_R, KC_T,                                   KC_Y, KC_U,KC_I, KC_O, KC_P, KC_BSLS,
    KC_LSFT, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H, LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_QUOT,
    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                                   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                KC_LGUI, KC_LALT, KC_SPC,                                   KC_ENT, LT(ADJUST,KC_TAB), KC_BSPC,
-                          LOWER,  KC_DEL,                                   KC_TAB, RAISE
+                KC_LGUI, KC_LALT, KC_SPC,                                   KC_ENT, LT(ADJUST, KC_TAB), LT(RAISE, KC_BSPC),
+                          LOWER,  KC_DEL,                                   KC_ESC, RAISE
 ),
 
 [_LOWER] = LAYOUT(
@@ -73,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
    KC_F11, KC_F1, KC_F2,  KC_F3, KC_F4, KC_F5,                       KC_F6, KC_F7,KC_F8, KC_F9, KC_F10, KC_F12,
    ____, ____, ____,  ____, ____, ____,                          ____, ____, KC_NUMLOCK, KC_INS, KC_SLCK, ____,
-   KC_MUTE, KC_VOLD, KC_VOLU, KC_BRIU , ____, ____,               KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ____, ____,
-   KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, ____, ____,                   ____, ____, ____, ____, ____, ____,
+   ____, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRIU , ____,                KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ____, ____,
+   ____, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, ____,                    ____, ____, ____, ____, ____, ____,
                                   ____, ____, ____,                  ____, ____, ____,
                                        ____,  ____,                  ____, ____
 ),
